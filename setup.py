f231ca956e1a2dbad0653f47c458aa79556d1dc6
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-12-14 15:02:17 -0800

    include config files

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="games_learning",
+    version="1.0.0",
+    packages=["games_learning"],
+    long_description=read("README.md"),
+)
