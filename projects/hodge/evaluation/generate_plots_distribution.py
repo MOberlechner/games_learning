539df88b64ba61bd99bae7ba8433deda8c92daa0
matthias <>
2023-12-30 10:26:56 +0100

    include plots with pure strict NE

--- projects/hodge/evaluation/generate_plots_distribution.py
+++ projects/hodge/evaluation/generate_plots_distribution.py
@@ -20,9 +20,9 @@ def set_axis(xlim, ylim, title, xlabel: str = "", ylabel: str = ""):
     ax.set_title(title, fontsize=FONTSIZE_TITLE)
     return fig, ax
 
-
-def generate_plot_random():
+def plot_distribution_potentialness():
     """plot of distribution of potentialness for randomly generated games"""
+    # Parameter
     n_bins = 50
     distribution = "uniform"
     fig, ax = set_axis((0, 1), (-0.001, 0.37), "", "Potentialness", "Density")
@@ -54,11 +54,11 @@ def generate_plot_random():
         for j in range(2)
     ]
     legend1 = ax.legend(
-        handles=line_styles, loc=1, frameon=False, fontsize=FONTSIZE_LEGEND
+        handles=line_styles, loc=5, frameon=False, fontsize=FONTSIZE_LEGEND
     )
     legend1.set_title("# Actions", prop={"size": FONTSIZE_LEGEND})
     legend2 = ax.legend(
-        handles=color_styles, loc=2, frameon=False, fontsize=FONTSIZE_LEGEND
+        handles=color_styles, loc=6, frameon=False, fontsize=FONTSIZE_LEGEND
     )
     legend2.set_title("# Agents", prop={"size": FONTSIZE_LEGEND})
 
@@ -70,6 +70,65 @@ def generate_plot_random():
     fig.savefig(f"{path_save}.{FORMAT}", bbox_inches="tight")
 
 
+def plot_distribution_psne():
+    """plot distribution of pure strict Nash equilibria (psne) w.r.t. potentialness for randomly generated games"""
+    # Parameter
+    n_bins = 20
+    distribution = "uniform"
+    fig, ax = set_axis((0, 1), (-0.001, 1.001), "", "Potentialness", "Existence PSNE")
+
+
+    # plot data
+    for j, n_agents in enumerate([2, 3]):
+        for i, n_actions in enumerate([2, 3, 4, 5]):
+
+            file_name = f"{distribution}_{[n_actions]*n_agents}.csv"
+            df = pd.read_csv(os.path.join(PATH_TO_DATA, "random", file_name))
+
+            # prepare data (group by potentialness and probability of strict NE)
+            df["bin"] = map_potentialness_to_bin(df["potentialness"], n_bins)
+            df["psne"] = df["n_strict_ne"] > 0
+            df = df.groupby(["bin"]).agg({"psne": "mean"}).reset_index() 
+            df["potentialness"] = [ map_bin_to_potentialness(b, n_bins) for b in df["bin"]]
+
+            # visualize
+            ax.plot(
+                df["potentialness"],
+                df["psne"],
+                linewidth=2,
+                color=COLORS[j],
+                linestyle=LS[i],
+                zorder=3 - j,
+            )
+
+    # create legend
+    line_styles = [
+        plt.Line2D([0], [0], color="black", linestyle=LS[i], linewidth=1.5, label=i + 2)
+        for i in range(4)
+    ]
+    color_styles = [
+        plt.Line2D([0], [0], color=COLORS[j], linestyle="-", linewidth=2, label=j + 2)
+        for j in range(2)
+    ]
+    legend1 = ax.legend(
+        handles=line_styles, loc=5, frameon=False, fontsize=FONTSIZE_LEGEND
+    )
+    legend1.set_title("# Actions", prop={"size": FONTSIZE_LEGEND})
+    legend2 = ax.legend(
+        handles=color_styles, loc=6, frameon=False, fontsize=FONTSIZE_LEGEND
+    )
+    legend2.set_title("# Agents", prop={"size": FONTSIZE_LEGEND})
+
+    # Display both legends on the plot
+    ax.add_artist(legend1)
+    ax.add_artist(legend2)
+    
+    path_save = os.path.join(PATH_TO_RESULTS, "random_psne")
+    fig.savefig(f"{path_save}.{FORMAT}", bbox_inches="tight")
+
+
+
 if __name__ == "__main__":
     os.makedirs(os.path.join(PATH_TO_RESULTS), exist_ok=True)
-    generate_plot_random()
+    plot_distribution_potentialness()
+    plot_distribution_psne()
