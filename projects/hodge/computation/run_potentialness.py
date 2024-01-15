582e2dc67a5129b5f311f49d81c24ed9c0e3a179
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-01-15 09:26:11 +0100

    update script

--- projects/hodge/computation/run_potentialness.py
+++ projects/hodge/computation/run_potentialness.py
@@ -9,7 +9,7 @@ from decomposition.game import Game
 from tqdm import tqdm
 
 from games_learning.game.econ_game import FPSB, SPSB, AllPay, Contest, EconGame
-from games_learning.game.matrix_game import RandomMatrixGame
+from games_learning.game.matrix_game import MatrixGame, RandomMatrixGame
 from games_learning.learner.learner import MirrorAscent
 from games_learning.simulation import Simulator
 from games_learning.utils.equil import find_pure_nash_equilibrium
@@ -17,18 +17,52 @@ from projects.hodge.configs import *
 from projects.hodge.util import save_result
 
 
-def run_sample_potentialness(
-    n_actions: int, n_samples: int, distribution: str, compute_equil: bool = False
+def run_matrix_potentialness():
+    """compute potentialness for different interesting matrix games"""
+    settings = {
+        "shapley_game": [
+            np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+            np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]]),
+        ],
+    }
+
+    data = deque()
+    for key, payoff_matrix in settings.items():
+        n_agents = len(payoff_matrix)
+        actions = list(payoff_matrix[0].shape)
+
+        game = MatrixGame(len(payoff_matrix), payoff_matrix, name=key)
+        pure_equil = find_pure_nash_equilibrium(game)
+
+        hodge = Game(actions, save_load=False)
+        hodge.compute_decomposition_matrix(game.payoff_matrix)
+        potentialness = hodge.metric
+
+        result = {
+            "name": key,
+            "actions": actions,
+            "potentialness": potentialness,
+            "n_weak_ne": len(pure_equil["weak_ne"]),
+            "n_strict_ne": len(pure_equil["strict_ne"]),
+            "weak_ne": pure_equil["weak_ne"],
+            "strict_new": pure_equil["strict_ne"],
+        }
+        data.append(result)
+
+    # save results
+    save_result(data, "matrix_games", f"potentialness.csv", PATH_TO_DATA)
+
+
+def run_random_potentialness(
+    actions: List[int], n_samples: int, distribution: str, compute_equil: bool = False
 ):
     """create random games and check potentialness"""
     data = deque()
-    hodge = Game(n_actions, save_load=False)
-    n_agents = len(n_actions)
+    hodge = Game(actions, save_load=False)
+    n_agents = len(actions)
 
     for seed in tqdm(range(n_samples)):
-        game = RandomMatrixGame(
-            n_agents, n_actions, seed=seed, distribution=distribution
-        )
+        game = RandomMatrixGame(n_agents, actions, seed=seed, distribution=distribution)
         hodge.compute_decomposition_matrix(game.payoff_matrix)
         potentialness = hodge.metric
         result = {
@@ -39,7 +73,7 @@ def run_sample_potentialness(
             pure_equil = find_pure_nash_equilibrium(game)
             equilibria = {
                 "n_weak_ne": len(pure_equil["weak_ne"]),
-                "n_strict_ne": len(pure_equil["strict_new"]),
+                "n_strict_ne": len(pure_equil["strict_ne"]),
             }
             result.update(equilibria)
 
@@ -47,30 +81,30 @@ def run_sample_potentialness(
         data.append(result)
 
     # save results
-    save_result(data, "random", f"{distribution}_{n_actions}.csv", PATH_TO_DATA)
+    save_result(data, "random", f"{distribution}_{actions}.csv", PATH_TO_DATA)
 
 
 def run_econgames_potentialness(
-    list_n_actions: List[int],
+    list_actions: List[int],
     interval: tuple = (0.00, 0.95),
     compute_equil: bool = False,
 ):
     """compute potentialness for econgames"""
     data = deque()
 
-    for n_actions in tqdm(list_n_actions):
+    for actions in tqdm(list_actions):
 
         # prepare decomposition
-        n_agents = len(n_actions)
-        n_discr = n_actions[0]
-        hodge = Game(n_actions, save_load=False)
+        n_agents = len(actions)
+        n_actions = actions[0]
+        hodge = Game(actions, save_load=True, path=PATH_TO_DATA)
 
         # create game
         games = [
-            FPSB(n_agents, n_discr, interval=interval),
-            SPSB(n_agents, n_discr, interval=interval),
-            AllPay(n_agents, n_discr, interval=interval),
-            Contest(n_agents, n_discr, interval=interval, csf_param=1.0),
+            FPSB(n_agents, n_actions, interval=interval),
+            SPSB(n_agents, n_actions, interval=interval),
+            AllPay(n_agents, n_actions, interval=interval),
+            Contest(n_agents, n_actions, interval=interval, csf_param=1.0),
         ]
 
         for game in games:
@@ -80,7 +114,7 @@ def run_econgames_potentialness(
             result = {
                 "game": game.name,
                 "n_agents": n_agents,
-                "n_discr": n_discr,
+                "n_discr": n_actions,
                 "interval": interval,
                 "potentialness": potentialness,
             }
@@ -102,24 +136,23 @@ def run_econgames_potentialness(
 
 if __name__ == "__main__":
 
-    for n_agents in [2, 3]:
-        for n in [2, 3, 4, 5]:
-            run_sample_potentialness(
-                n_actions=[n] * n_agents,
-                n_samples=1_000_000,
-                distribution="uniform",
-                compute_equil=True,
-            )
+    # for n_agents in [2, 3]:
+    #     for n_actions in [2, 3, 4, 5]:
+    #         run_random_potentialness(
+    #             actions=[n_actions] * n_agents,
+    #             n_samples=1_000_000,
+    #             distribution="uniform",
+    #             compute_equil=True,
+    #         )
 
-    list_n_actions = [
+    list_actions = [
         [5, 5],
         [8, 8],
         [11, 11],
         [16, 16],
+        [20, 20],
         [24, 24],
         [5, 5, 5],
         [8, 8, 8],
     ]
-    run_econgames_potentialness(
-        list_n_actions, interval=(0.00, 0.95), compute_equil=True
-    )
+    run_econgames_potentialness(list_actions, interval=(0.05, 0.95), compute_equil=True)
