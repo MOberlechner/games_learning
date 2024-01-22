0105c79358931290ec5abed7d496daa6fb986211
matthias <>
2024-01-22 17:55:45 +0100

    separate scripts for econgames and random games

--- projects/hodge/computation/run_random_potentialness.py
+++ projects/hodge/computation/run_random_potentialness.py
@@ -1,11 +1,19 @@
+import multiprocessing
 import os
+import sys
 from collections import deque
+from datetime import datetime
+from functools import partial
+from itertools import chain, product
+from typing import List
 
+import numpy as np
 import pandas as pd
 from decomposition.game import Game
 from tqdm import tqdm
 
-from games_learning.game.matrix_game import RandomMatrixGame
+from games_learning.game.econ_game import FPSB, SPSB, AllPay, Contest, EconGame
+from games_learning.game.matrix_game import MatrixGame, RandomMatrixGame
 from games_learning.learner.learner import MirrorAscent
 from games_learning.simulation import Simulator
 from games_learning.utils.equil import find_pure_nash_equilibrium
@@ -13,46 +21,98 @@ from projects.hodge.configs import *
 from projects.hodge.util import save_result
 
 
-def run_sample_potentialness(
-    n_actions: int, n_samples: int, distribution: str, compute_equil: bool = False
+def run_random_potentialness(
+    seeds: List[int], actions: List[int], distribution: str, compute_equil: bool = False, flow: bool = False
 ):
     """create random games and check potentialness"""
-    data = deque(maxlen=n_samples)
-    hodge = Game(n_actions, save_load=False)
-    n_agents = len(n_actions)
+    data = deque()
+    hodge = Game(
+        actions, 
+        save_load=False, 
+        path="/home/matthias/Git/MOberlechner/matrix_game_learning/projects/hodge/data/"
+    )
+    n_agents = len(actions)
 
-    for seed in tqdm(range(n_samples)):
-        game = RandomMatrixGame(
-            n_agents, n_actions, seed=seed, distribution=distribution
-        )
-        hodge.compute_decomposition_matrix(game.payoff_matrix)
-        potentialness = hodge.metric
-        result = {
-            "seed": seed,
-            "potentialness": potentialness,
-        }
+    for seed in tqdm(seeds):
+        game = RandomMatrixGame(n_agents, actions, seed=seed, distribution=distribution)
+        if flow:
+            hodge.compute_flow_decomposition_matrix(game.payoff_matrix)
+            potentialness = hodge.flow_metric
+            result = {
+                "seed": seed,
+                "potentialness_flow": potentialness,
+            }
+        else:
+            hodge.compute_decomposition_matrix(game.payoff_matrix)
+            potentialness = hodge.metric
+            result = {
+                "seed": seed,
+                "potentialness": potentialness,
+            }
+
         if compute_equil:
             pure_equil = find_pure_nash_equilibrium(game)
             equilibria = {
                 "n_weak_ne": len(pure_equil["weak_ne"]),
-                "n_strict_ne": len(pure_equil["strict_new"]),
+                "n_strict_ne": len(pure_equil["strict_ne"]),
             }
             result.update(equilibria)
 
         # log result
         data.append(result)
 
+    return data
+
+
+def run_random_potentialness_mp(
+    actions: List[int],
+    n_samples: int,
+    distribution: str,
+    compute_equil: bool = False,
+    flow: bool = False,
+    num_processes: int = 1,
+):
+    if num_processes > multiprocessing.cpu_count():
+        print(f"Only {multiprocessing.cpu_count()} process available")
+        num_processes = multiprocessing.cpu_count()
+
+    # create function that runs in parallel
+    func = partial(
+        run_random_potentialness,
+        actions=actions,
+        distribution=distribution,
+        compute_equil=compute_equil,
+        flow = flow,
+    )
+
+    # create seeds for different processes
+    n_seeds = n_samples // num_processes + 1
+    seeds = [
+        list(range(i * n_seeds, min((i + 1) * n_seeds, n_samples)))
+        for i in range(num_processes)
+    ]
+
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        results = pool.map(func, seeds)
+
     # save results
-    save_result(data, "random", f"{distribution}_{n_actions}.csv", PATH_TO_DATA)
+    data = deque(chain.from_iterable(results))
+    dir = "random_flow" if flow else "random"
+    save_result(data, dir, f"{distribution}_{actions}.csv", PATH_TO_DATA)
 
 
 if __name__ == "__main__":
 
-    for n_agents in [2, 3]:
-        for n in [2, 3, 4, 5]:
-            run_sample_potentialness(
-                n_actions=[n] * n_agents,
+    # compute potentialness for random games
+    for n_agents in [12]:
+        for n_actions in [2]:
+            actions=[n_actions] * n_agents
+            print(f"Experiment: {actions}")
+            run_random_potentialness_mp(
+                actions=actions,
                 n_samples=1_000_000,
                 distribution="uniform",
                 compute_equil=True,
+                flow=True,
+                num_processes=8,
             )
