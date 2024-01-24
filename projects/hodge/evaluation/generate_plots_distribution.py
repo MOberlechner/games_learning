f1e7181f67d363586610273005f1d66edd4e5ee9
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-01-24 05:04:17 +0100

    update plots for random games

--- projects/hodge/evaluation/generate_plots_distribution.py
+++ projects/hodge/evaluation/generate_plots_distribution.py
@@ -118,8 +118,8 @@ def plot_distribution_potentialness(list_n_agents, list_n_actions, name, dir):
     fig.savefig(f"{path_save}.{FORMAT}", bbox_inches="tight")
 
 
-def plot_distribution_spne(list_n_agents, list_n_actions, name, dir):
-    """plot distribution of strict pure Nash equilibria (spne) w.r.t. potentialness for randomly generated games"""
+def plot_potentialness_vs_spne(list_n_agents, list_n_actions, name, dir):
+    """plot probability of existence of strict pure Nash equilibria (spne) w.r.t. potentialness for randomly generated games"""
     # Parameter
     n_bins = 25
     distribution = "uniform"
@@ -198,17 +198,120 @@ def plot_distribution_spne(list_n_agents, list_n_actions, name, dir):
     fig.savefig(f"{path_save}.{FORMAT}", bbox_inches="tight")
 
 
+def plot_probability_spne(list_n_agents, list_n_actions, name, dir):
+    """plot probability of existence of strict pure Nash equilibria (spne) for randomly generated games"""
+    # Parameter
+    n_bins = 25
+    distribution = "uniform"
+    fig, ax = set_axis((0, 1), (-0.001, 1.001), "", "Potentialness", "P(SPNE exists)")
+
+    # plot data
+    for i, n_agents in enumerate(list_n_agents):
+        for j, n_actions in enumerate(list_n_actions):
+            try:
+                file_name = f"{distribution}_{[n_actions]*n_agents}.csv"
+                path = os.path.join(PATH_TO_DATA, dir, file_name)
+                df = pd.read_csv(path)
+
+                # prepare data (group by potentialness and probability of strict NE)
+                metric = (
+                    "potentialness"
+                    if "potentialness" in df.columns
+                    else "potentialness_flow"
+                )
+                if "n_strict_ne" in df.columns:
+                    df["spne"] = df["n_strict_ne"] > 0
+                    y = df["spne"].mean()
+                    x0 = df[metric].min()
+                    x1 = df[metric].max()
+                    x_mean = df[metric].mean()
+
+                    # visualize
+                    ax.plot(
+                        [x0, x1],
+                        [y, y],
+                        linewidth=2,
+                        color=get_colors(i, len(list_n_agents)),
+                        linestyle=LS[j],
+                        zorder=i,
+                    )
+                    ax.scatter(
+                        [x_mean],
+                        [y],
+                        linewidth=2,
+                        color=get_colors(i, len(list_n_agents)),
+                        marker="o",
+                        s=25,
+                        zorder=i,
+                    )
+                else:
+                    print(
+                        f"number of SPNE not avaliable for setting {[n_actions]*n_agents}"
+                    )
+            except:
+                print(f"Setting {[n_actions]*n_agents} not available (path:{path})")
+
+    plt.axhline(y=1 - 1 / np.exp(1), color="k", linestyle="--", linewidth=0.7)
+    plt.text(y=1 - 1 / np.exp(1) - 0.06, x=0.8, s=r"$1-\dfrac{1}{e}$")
+
+    # create legend
+    line_styles = [
+        plt.Line2D(
+            [0], [0], color="black", linestyle=LS[j], linewidth=1.5, label=n_actions
+        )
+        for j, n_actions in enumerate(list_n_actions)
+    ]
+    color_styles = [
+        plt.Line2D(
+            [0],
+            [0],
+            color=get_colors(i, len(list_n_agents)),
+            linestyle="-",
+            linewidth=2,
+            label=n_agents,
+        )
+        for i, n_agents in enumerate(list_n_agents)
+    ]
+    legend1 = ax.legend(
+        handles=line_styles, loc=4, frameon=False, fontsize=FONTSIZE_LEGEND
+    )
+    legend1.set_title("# Actions", prop={"size": FONTSIZE_LEGEND})
+    legend2 = ax.legend(
+        handles=color_styles, loc=3, frameon=False, fontsize=FONTSIZE_LEGEND
+    )
+    legend2.set_title("# Agents", prop={"size": FONTSIZE_LEGEND})
+
+    # Display both legends on the plot
+    ax.add_artist(legend1)
+    ax.add_artist(legend2)
+
+    path_save = os.path.join(PATH_TO_RESULTS, name)
+    fig.savefig(f"{path_save}.{FORMAT}", bbox_inches="tight")
+
+
 if __name__ == "__main__":
     os.makedirs(os.path.join(PATH_TO_RESULTS), exist_ok=True)
+
+    # distribution potentialness
     plot_distribution_potentialness(
         list_n_agents=[2, 4, 8, 10],
         list_n_actions=[2, 4, 12, 24],
         name="random_potentialness_1e5",
         dir="random_flow_1e5",
     )
-    plot_distribution_spne(
+
+    # relation potentialness and SPNE
+    plot_potentialness_vs_spne(
         list_n_agents=[2, 4, 8, 10],
         list_n_actions=[2, 4, 12, 24],
         name="random_spne_1e5",
+        dir="random_flow_1e5",
+    )
+
+    # relation potentialness and SPNE
+    plot_probability_spne(
+        list_n_agents=[2, 4, 8, 10],
+        list_n_actions=[2, 4, 12, 24],
+        name="random_prob_spne_1e5",
         dir="random_flow_1e5",
     )
