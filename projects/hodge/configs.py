18cf6db0f5f910cda9357bbd167d24fb3454f0c0
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-01-31 10:47:43 +0100

    update config and util

--- projects/hodge/configs.py
+++ projects/hodge/configs.py
@@ -5,11 +5,8 @@ MAX_ITER = 2_000
 TOL = 1e-8
 
 # Step-sizes
-LIST_ETA = [2**i for i in [8, 4, 0, -4]]
+LIST_ETA = [2**i for i in [8, 4]]
 LIST_BETA = [0.05, 0.5]
-
-LIST_ETA = [2**4]
-LIST_BETA = [0.5]
 
 # Settings
 SETTINGS = [(2, 2), (2, 4), (2, 12), (2, 24), (4, 2), (4, 4), (8, 2), (10, 2)]
@@ -24,5 +21,5 @@ LS = ["solid", "dashed", "dashdot", "dotted"]
 FONTSIZE_LABEL = 13
 FONTSIZE_TITLE = 13
 FONTSIZE_LEGEND = 13
-FORMAT = "png"
+FORMAT = "pdf"
 DPI = 300
