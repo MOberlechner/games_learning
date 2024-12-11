ebbbc52bc90c0f02783712db5a0950f0a1a7e1d4
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-12-11 16:36:21 +0100

    moved computation of potentialness to existing script

--- projects/hodge/computation/run_econ_games_potentialness.py
+++ projects/hodge/computation/run_econ_games_potentialness.py
@@ -1,6 +1,8 @@
-import multiprocessing
 import os
 import sys
+
+sys.path.append(os.path.realpath("."))
+
 from collections import deque
 from datetime import datetime
 from functools import partial
@@ -12,6 +14,7 @@ import pandas as pd
 from games_decomposition.game import Game
 from tqdm import tqdm
 
+from games_learning.game.bayesian_game import BayesianEconGame
 from games_learning.game.econ_game import (
     FPSB,
     SPSB,
@@ -33,10 +36,83 @@ def get_valuations(setting: str, n_agents: int) -> Tuple[float]:
         raise ValueError(f"valuation setting '{setting}' not available")
 
 
+def get_types(n_types: int) -> List:
+    if n_types == 1:
+        return np.array([1.0])
+    elif n_types == 2:
+        return np.array([0.5, 1])
+    elif n_types == 3:
+        return np.array([0.3, 0.6, 1.0])
+    elif n_types == 4:
+        return np.array([0.25, 0.50, 0.75, 1.0])
+    else:
+        return None
+
+
+def get_number_strategies(n_types, n_actions) -> int:
+    return math.comb(n_types + n_actions - 1, n_actions - 1)
+
+
+def run_econgames_bayesian_potentialness(
+    list_n_types: List[int], compute_equil: bool = False
+):
+    """compute potentialness of bayesian version of economic games"""
+    # create complete-information games
+    n_agents = 2
+    valuations = (1.0, 1.0)
+    n_actions = 4
+    interval = (0.0, 0.95)
+    games = [
+        FPSB(n_agents, n_actions, valuations=valuations, interval=interval),
+        SPSB(n_agents, n_actions, valuations=valuations, interval=interval),
+        AllPay(n_agents, n_actions, valuations=valuations, interval=interval),
+        WarOfAttrition(n_agents, n_actions, valuations=valuations, interval=interval),
+        Contest(
+            n_agents, n_actions, valuations=valuations, interval=interval, csf_param=1.0
+        ),
+    ]
+
+    data = deque()
+    for n_types in list_n_types:
+        types = get_types(n_types)
+        n_strategies = get_number_strategies(n_types, n_actions)
+        hodge = Game([n_strategies] * n_agents, save_load=True, path=PATH_TO_DATA)
+        for game in games:
+            bayesian_game = BayesianEconGame(
+                game, types=types, distribution="uniform", monotone_strategies=True
+            )
+            hodge.compute_flow_decomposition_matrix(bayesian_game.payoff_matrix)
+            potentialness = hodge.flow_metric
+            result = {
+                "game": bayesian_game.name,
+                "n_agents": n_agents,
+                "n_actions": n_actions,
+                "n_types": n_types,
+                "n_strategies": bayesian_game.n_actions[0],
+                "potentialness": potentialness,
+            }
+            # compute pure equilibria
+            if compute_equil:
+                pure_equil = bayesian_game.get_pne()
+                equilibria = {
+                    "n_weak_ne": len(pure_equil["weak_ne"]),
+                    "n_strict_ne": len(pure_equil["strict_ne"]),
+                }
+                result.update(equilibria)
+            # log result
+            result.update({"timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+            data.append(result)
+
+    # save results
+    save_result(
+        data, "econgames", f"potentialness_bayesian.csv", PATH_TO_DATA, overwrite=False
+    )
+
+
 def run_econgames_potentialness(
     n_agents: int,
     list_n_discr: List[int],
-    interval: tuple = (0.00, 0.95),
+    interval: tuple = (0.00, 1.0),
     compute_equil: bool = False,
 ):
     """compute potentialness for econgames"""
@@ -99,9 +175,14 @@ def run_econgames_potentialness(
 
 
 if __name__ == "__main__":
-    # compute potentialness for econcames
+    # compute potentialness for econgames
     n_agents = 2
-    list_n_discr = range(5, 26)
+    list_n_discr = range(3, 26)
     run_econgames_potentialness(
         n_agents, list_n_discr, interval=(0.0, 0.95), compute_equil=True
     )
+
+    # compute potentialness for bayesian econgames
+    list_n_types = [1, 2, 3, 4]
+    compute_equil = True
+    run_econgames_bayesian_potentialness(list_n_types, compute_equil)
