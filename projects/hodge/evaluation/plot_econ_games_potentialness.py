2520d92956b75bc0ea90137fe3397409a99484e0
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-12-11 16:37:18 +0100

    update plotting of econgames

--- projects/hodge/evaluation/plot_econ_games_potentialness.py
+++ projects/hodge/evaluation/plot_econ_games_potentialness.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.append(os.path.realpath("."))
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,55 +11,115 @@ from projects.hodge.configs import *
 from projects.hodge.util import *
 
 
-def plot_potentialness_discretization(list_n_discr):
+def plot_potentialness_bayesian(list_n_types, games, label_games):
+    # import data
+    df = pd.read_csv(f"{PATH_TO_DATA}econgames/potentialness_bayesian.csv")
+
+    fig, ax = set_axis(
+        (0.5, list_n_types[-1] + 0.5),
+        (0, 1),
+        xlabel="Number of Types",
+        ylabel="Potentialness",
+    )
+    for i in range(len(games)):
+        y = [
+            df.loc[(df.game == games[i]) & (df.n_types == t), "potentialness"].item()
+            for t in list_n_types
+        ]
+        ax.plot(
+            list_n_types,
+            y,
+            color=get_colors(i, len(games)),
+            linewidth=2,
+            marker=MARKER[i],
+            label=label_games[i],
+        )
+    ax.set_xticks(list_n_types)
+
+    # add legend
+    legend1 = ax.legend(
+        loc="center left",
+        bbox_to_anchor=(1.0, 0.5),
+        frameon=False,
+        fontsize=FONTSIZE_LEGEND,
+        ncol=1,
+    )
+    legend1.set_title("Games", prop={"size": FONTSIZE_LEGEND})
+    ax.add_artist(legend1)
+
+    path_save = os.path.join(PATH_TO_RESULTS, "econgames_bayesian")
+    fig.savefig(
+        f"{path_save}.{FORMAT}", bbox_inches="tight", bbox_extra_artists=(legend1,)
+    )
+    print(f"figure: '{path_save}.{FORMAT}' created")
+
+
+def plot_potentialness_discretization(list_n_discr, games, label_games):
     """Plot Potentialness of different econgames w.r.t. discretization"""
     # Parameter
     interval = (0.0, 0.95)
     val_settings = ["symmetric", "asymmetric"]
     label_val_settings = [r"sym.   ($v_1=1.0, v_2=1.0$)", r"asym. ($v_1=0.8, v_2=1.0$)"]
-    games = ["contest", "spsb", "fpsb", "allpay"]
-    labels = ["Contest", "SPSB", "FPSB", "All-Pay"]
     # import data
     df = pd.read_csv(f"{PATH_TO_DATA}econgames/potentialness.csv")
     df = df[df.interval == str(interval)]
 
-    fig, ax = set_axis((4, 26), (0, 1), xlabel="Discretization", ylabel="Potentialness")
+    fig, ax = set_axis(
+        (list_n_discr[0] - 1, list_n_discr[-1] + 1),
+        (0, 1),
+        xlabel="Discretization",
+        ylabel="Potentialness",
+    )
     for i, game in enumerate(games):
         for j, valuations in enumerate(val_settings):
             tmp = df[
                 (df.game == game) & (df.n_agents == 2) & (df.valuation == valuations)
             ]
             ax.plot(
-                tmp.n_discr,
-                tmp.potentialness,
-                # marker=".",
-                # markersize=10,
-                linewidth=2.5,
+                list_n_discr,
+                [tmp[tmp.n_discr == n].potentialness.item() for n in list_n_discr],
+                # marker=MARKER[i],
+                # markersize=5,
+                linewidth=2,
                 color=get_colors(i, len(games)),
                 linestyle=LS[j],
+                label=label_games[i] if j == 0 else None,
             )
     ax.set_xticks([5, 10, 15, 20, 25])
 
-    # add legends
-    legend1, legend2 = create_legend(
-        ax,
-        labels,
-        [],
-        position=("upper center", "center right"),
-        label1="",
-        label2="",
-        ncols1=2,
-        ncols2=1,
+    # add legend
+    legend1 = ax.legend(
+        loc="center left",
+        bbox_to_anchor=(1.0, 0.5),
+        frameon=False,
+        fontsize=FONTSIZE_LEGEND,
+        ncol=1,
     )
+    legend1.set_title("Games", prop={"size": FONTSIZE_LEGEND})
     ax.add_artist(legend1)
-    ax.add_artist(legend2)
 
     path_save = os.path.join(PATH_TO_RESULTS, "econgames_discr")
-    fig.savefig(f"{path_save}.{FORMAT}", bbox_inches="tight")
+    fig.savefig(
+        f"{path_save}.{FORMAT}", bbox_inches="tight", bbox_extra_artists=(legend1,)
+    )
     print(f"figure: '{path_save}.{FORMAT}' created")
 
 
 if __name__ == "__main__":
     os.makedirs(os.path.join(PATH_TO_RESULTS), exist_ok=True)
-    list_n_discr = list(range(5, 26))
-    plot_potentialness_discretization(list_n_discr)
+
+    list_n_discr = list(range(4, 26))
+    games = ["contest", "spsb", "fpsb", "war_of_attrition", "allpay"]
+    label_games = ["Contest", "SPSB", "FPSB", "WoA", "Allpay"]
+    plot_potentialness_discretization(list_n_discr, games, label_games)
+
+    list_n_types = [1, 2, 3, 4]
+    games = [
+        "bayesian_contest",
+        "bayesian_spsb",
+        "bayesian_fpsb",
+        "bayesian_war_of_attrition",
+        "bayesian_allpay",
+    ]
+    label_games = ["Contest", "SPSB", "FPSB", "WoA", "Allpay"]
+    plot_potentialness_bayesian(list_n_types, games, label_games)
